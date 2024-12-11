b79ba34b1d30e69d54e8399f21fa17157fb983b9
Mustakim <mustakim_bellah@yahoo.com>
2024-12-11 16:15:23 +0600

    added sustainability

--- src/components/OurBusin/sustainabilityInitiatives.jsx
+++ src/components/OurBusin/sustainabilityInitiatives.jsx
@@ -4,13 +4,13 @@ import "./OurBusinessStyle.css";
 
 const sustainabilityInitiatives = () => {
   return (
-    <div>
+    <>
       <div className="container my-3">
-        <h3 className="fw-bolder fs-3">Sustainability Initiatives</h3>
+        <h5 className="fw-bolder fs-3">Sustainability Initiatives</h5>
       </div>
       <div className="photo-text-business">
         <div className="photo-text-overlay-business">
-          <p className="fs-5 fw-bolder mx-3">
+          <p className="fw-bolder mx-4">
             HAMS is committed to sustainability, as reflected in its
             achievements and future goals. The company has adopted renewable
             energy sources, upgraded its effluent treatment plant, and committed
@@ -25,16 +25,47 @@ const sustainabilityInitiatives = () => {
           alt="Sustainability Initiatives"
           className="img-fluid d-none d-md-block"
         />
-
         <img
           src={Sustainability}
           alt="Sustainability Initiatives"
           className="img-fluid d-block d-md-none"
-          style={{ width: "100%", height: "700px" }}
+          style={{ width: "100%", height: "500px" }}
         />
       </div>
-    </div>
+    </>
   );
 };
 
 export default sustainabilityInitiatives;
+
+// ========  previous code ==========
+
+// <div>
+//   <div className="container my-3">
+//     <h3 className="fw-bolder fs-3">Sustainability Initiatives</h3>
+//   </div>
+//   <div className="photo-text-business">
+//     <div className="photo-text-overlay-business">
+//       <p className="fs-5 fw-bolder mx-3">
+//         HAMS is committed to sustainability, as reflected in its achievements
+//         and future goals. The company has adopted renewable energy sources,
+//         upgraded its effluent treatment plant, and committed to significant
+//         reductions in energy, greenhouse gas emissions, and water consumption by
+//         2030. Accreditations from FEM and FSLM underscore its commitment to
+//         environmental and social responsibility.
+//       </p>
+//     </div>
+//     <img
+//       src={Sustainability}
+//       alt="Sustainability Initiatives"
+//       className="img-fluid d-none d-md-block"
+//     />
+
+//     <img
+//       src={Sustainability}
+//       alt="Sustainability Initiatives"
+//       className="img-fluid d-block d-md-none"
+//       style={{ width: "100%", height: "700px" }}
+//     />
+//   </div>
+// </div>;
